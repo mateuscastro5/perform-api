d6bfcb9d4bd1af1908ed6be3d645e7c4f5d6e409
Mateus PS <mateus.castro2011@gmail.com>
2025-11-30 14:25:14 -0300

    fix: register entities directly in AppModule for Vercel

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ScheduleModule } from '@nestjs/schedule';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { dataSourceOptions } from './database/data-source';
 import { UsersModule } from './users/users.module';
 import { SquadsModule } from './squads/squads.module';
 import { DevelopersModule } from './developers/developers.module';
@@ -19,6 +18,42 @@ import { GithubWebhookModule } from './github-webhook/github-webhook.module';
 import { GithubModule } from './github/github.module';
 import { AuthModule } from './auth/auth.module';
 
+import { User } from './users/entities/user.entity';
+import { Squad } from './squads/entities/squad.entity';
+import { Developer } from './developers/entities/developer.entity';
+import { PullRequest } from './pull-requests/entities/pull-request.entity';
+import { Commit } from './commits/entities/commit.entity';
+import { CodeReview } from './code-reviews/entities/code-review.entity';
+import { Metric } from './metrics/entities/metric.entity';
+import { Report } from './reports/entities/report.entity';
+import { Permission } from './permissions/entities/permission.entity';
+import { AiRecommendation } from './ai-recommendations/entities/ai-recommendation.entity';
+import { GithubWebhook } from './github-webhook/entities/github-webhook.entity';
+import { MonitoredRepository } from './github/entities/monitored-repository.entity';
+import { GithubPullRequest } from './github/entities/github-pull-request.entity';
+import { GithubPrReview } from './github/entities/github-pr-review.entity';
+import { GithubConfiguration } from './github/entities/github-configuration.entity';
+import { GithubCommit } from './github/entities/github-commit.entity';
+
+const entities = [
+  User,
+  Squad,
+  Developer,
+  PullRequest,
+  Commit,
+  CodeReview,
+  Metric,
+  Report,
+  Permission,
+  AiRecommendation,
+  GithubWebhook,
+  MonitoredRepository,
+  GithubPullRequest,
+  GithubPrReview,
+  GithubConfiguration,
+  GithubCommit,
+];
+
 @Module({
   imports: [
     ConfigModule.forRoot({
@@ -28,7 +63,18 @@ import { AuthModule } from './auth/auth.module';
 
     ScheduleModule.forRoot(),
 
-    TypeOrmModule.forRoot(dataSourceOptions),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.get<string>('DATABASE_URL'),
+        entities: entities,
+        synchronize: false,
+        ssl: { rejectUnauthorized: false },
+        logging: false,
+      }),
+    }),
 
     UsersModule,
 
